43658df0582d7410ca34949323964ff38ca22ac1
dreamdye <2254674706@qq.com>
2025-05-20 19:50:05 +0800

    @dreamdye

--- src/main/java/top/playereg/sys/pages/MangerInterface.java
+++ src/main/java/top/playereg/sys/pages/MangerInterface.java
@@ -1,5 +1,6 @@
 package top.playereg.sys.pages;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -16,19 +17,42 @@ public class MangerInterface {
     public  void init(){
     //给窗口设置属性
         jf.setSize(WIDTH, HEIGHT);
-       jf.setLocationRelativeTo(null);
+        jf.setLocationRelativeTo(null);
         jf.setResizable(false);
         jf.setIconImage(new ImageIcon("src/main/java/top/playereg/sys/img/logo.jpg").getImage());
 
 
         //设置菜单栏
         JMenuBar jmb = new JMenuBar();
-        JMenu jMenu = new JMenu("系统设置");
-        JMenuItem jmi1 = new JMenuItem("切换账号");
-        JMenuItem jmi2 = new JMenuItem("退出程序");
+
+        JMenuItem jmi1 = new JMenuItem("      切换账号");
+        JMenuItem jmi2 = new JMenuItem("      退出");
+        // 设置最大宽度和高度
+        jmi1.setMaximumSize(new Dimension(100, 30));
+        jmi2.setMaximumSize(new Dimension(100, 30));
+
+        // 设置首选尺寸（部分 LookAndFeel 支持）
+        jmi1.setPreferredSize(new Dimension(100, 30));
+        jmi2.setPreferredSize(new Dimension(100, 30));
+
+        // 设置字体居中
+        jmi1.setHorizontalAlignment(SwingConstants.CENTER);
+        jmi2.setHorizontalAlignment(SwingConstants.CENTER);
+
 
         //添加菜单项
         //切换账号等登录完善
+        jmi1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    jf.dispose();
+                    new LoginFrame();
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
 
         //退出程序
         jmi2.addActionListener(new ActionListener() {
@@ -37,11 +61,17 @@ public class MangerInterface {
                 System.exit(0);
             }
         });
-        //JMenu.add(jmi1);
-        jMenu.add(jmi2);
-        jmb.add(jMenu);
+        jmb.add(jmi1);
+        jmb.add(Box.createHorizontalStrut(20));
+        jmb.add(jmi2);
+
 
         jf.setJMenuBar(jmb);
+
+        //设置分割面板
+        JSplitPane jsp = new JSplitPane();
+
+        //支持连续布局
 
 
 
