7957544669648ffed0230632041ce972b62ef8f3
PlayerEG <enderg@qq.com>
2025-05-20 13:20:00 +0800

    @PlayerEG

--- src/main/java/top/playereg/sys/Main.java
+++ src/main/java/top/playereg/sys/Main.java
@@ -5,6 +5,7 @@ import top.playereg.sys.pages.LoginFrame;
 public class Main {
     public static void main(String[] args) {
         System.out.println("欢迎使用系统");
+        System.out.println("正在加载...");
         new LoginFrame();
     }
 }
