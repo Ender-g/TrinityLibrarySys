949bf18804da3d0c81a44c3f30431f98c24217ec
PlayerEG <enderg@qq.com>
2025-05-20 19:33:12 +0800

    @PlayerEG

---
+++ src/main/java/top/playereg/sys/utils/diyColors.java
@@ -0,0 +1,14 @@
+package top.playereg.sys.utils;
+
+import java.awt.*;
+
+public class diyColors {
+    public static Color skyblue = new Color(0x87CEEB);
+    public static Color lightblue = new Color(0x00BFFF);
+    public static Color darkgreen = new Color(0x006400);
+    public static Color lightgreen = new Color(0x7CFC00);
+    public static Color transparent = new Color(0x000000, true);
+    // 保留原color4和color5
+    public static Color color4 = new Color(0xCD853F);
+    public static Color color5 = new Color(0xB22222);
+}
