1d7bef2bd689bdb5ff01a4c99e2bf2efb2ffcd90
dreamdye <2254674706@qq.com>
2025-05-20 23:38:45 +0800

    @dreamdye

--- src/main/java/top/playereg/sys/pages/MangerInterface.java
+++ src/main/java/top/playereg/sys/pages/MangerInterface.java
@@ -1,10 +1,17 @@
 package top.playereg.sys.pages;
 
+import com.sun.javafx.scene.shape.PathUtils;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
 
 public class MangerInterface {
     JFrame jf = new JFrame("图书馆：xxx，欢迎您");
@@ -19,7 +26,7 @@ public class MangerInterface {
         jf.setSize(WIDTH, HEIGHT);
         jf.setLocationRelativeTo(null);
         jf.setResizable(false);
-        jf.setIconImage(new ImageIcon("src/main/java/top/playereg/sys/img/logo.jpg").getImage());
+        jf.setIconImage(new ImageIcon("src/main/java/top/playereg/sys/img/book.png").getImage());
 
 
         //设置菜单栏
@@ -72,12 +79,34 @@ public class MangerInterface {
         JSplitPane jsp = new JSplitPane();
 
         //支持连续布局
+        jsp.setContinuousLayout(true);
+        jsp.setDividerLocation(200);
+        jsp.setDividerSize(10);
+
+
+        //设置左侧内容
+        DefaultMutableTreeNode  root = new DefaultMutableTreeNode("系统管理");
+        DefaultMutableTreeNode  userManage = new DefaultMutableTreeNode("用户管理");
+        DefaultMutableTreeNode  bookManage = new DefaultMutableTreeNode("图书管理");
+        DefaultMutableTreeNode  borrowManage = new DefaultMutableTreeNode("借阅管理");
+        DefaultMutableTreeNode  statisticsMange = new DefaultMutableTreeNode("统计分析");
+
+        root.add(userManage);
+        root.add(bookManage);
+        root.add(borrowManage);
+        root.add(statisticsMange);
+
+        JTree tree = new JTree(root);
+        jsp.setLeftComponent(tree);
+        jf.add(jsp);
+
 
 
 
 
 
         jf.setVisible(true);
+
 
 
     }
@@ -89,7 +118,40 @@ public class MangerInterface {
             e.printStackTrace();
         }
 
-
-
     }
+
+    private class MyRenderer extends DefaultTreeCellRenderer {
+        private Image rootIcon = null;
+        private Image userIcon = null;
+        private Image bookIcon = null;
+        private Image borrowIcon = null;
+        private Image statisticsIcon = null;
+            //ImageIO.read(new File(PathUtils.getResource("top/playereg/sys/img/book.png")));
+        public MyRenderer() {
+            try {
+                rootIcon = ImageIO.read(new File("src/main/java/top/playereg/sys/img/xitong.png"));
+                userIcon = ImageIO.read(new File("src/main/java/top/playereg/sys/img/yonghu.png"));
+                bookIcon = ImageIO.read(new File("src/main/java/top/playereg/sys/img/shu.png"));
+                borrowIcon = ImageIO.read(new File("src/main/java/top/playereg/sys/img/jieyue.png"));
+                statisticsIcon = ImageIO.read(new File("src/main/java/top/playereg/sys/img/tongji.png"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        //
+
+        @Override
+        public  Component getTreeCellRendererComponent(JTree tree, Object value,
+                                                      boolean sel, boolean expanded,
+                                                      boolean leaf, int row,
+                                                      boolean hasFocus) {
+            //使用默认绘制
+            super.getTreeCellRendererComponent(tree, value, sel, expanded,
+                    leaf, row, hasFocus);
+
+            return this;
+
+        }
+    }
+
 }
