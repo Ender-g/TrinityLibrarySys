191be471d94af91007a06734d22565240c710590
PlayerEG <enderg@qq.com>
2025-05-20 09:06:16 +0800

    @PlayerEG

--- src/main/java/top/playereg/sys/pages/LoginFrame.java
+++ src/main/java/top/playereg/sys/pages/LoginFrame.java
@@ -2,7 +2,16 @@ package top.playereg.sys.pages;
 
 import top.playereg.sys.utils.MakeFrameTool;
 
+import javax.swing.*;
+
 public class LoginFrame extends javax.swing.JFrame {
+    private JLabel loginpanel; // 登录面板
+    private JLabel emailLabel, passwordLabel, emailCodeLabel; // 邮箱、密码、验证码（文本内容）
+    private JTextField emailField, emailCodeField; // 邮箱、验证码（输入框）
+    private JPasswordField passwordField; // 密码（输入框）
+    private JButton loginBtn, registerBtn, forgetBtn; // 登录、注册、忘记密码（按钮）
+    private JLabel backgroundImg; // 背景图片
+
     public LoginFrame() {
         new MakeFrameTool(
                 "src/main/java/top/playereg/sys/img/book.png",
---
+++ src/main/java/top/playereg/sys/utils/SendEmailTool.java
@@ -0,0 +1,59 @@
+package top.playereg.sys.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.resend.Resend;
+import com.resend.core.exception.ResendException;
+import com.resend.services.emails.model.CreateEmailOptions;
+import com.resend.services.emails.model.CreateEmailResponse;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class SendEmailTool {
+    public static void main(String[] args) {
+        sendEmail(
+                "TestBot",
+                "test@email.playereg.top",
+                "enderg@qq.com",
+                "测试邮件",
+                "<html><body><h1>这是测试邮件</h1></body></html>"
+        );
+    }
+
+    public static boolean sendEmail(
+            String sendName,
+            String from,
+            String to,
+            String title,
+            String text
+    ) {
+        try {
+            // 使用 Gson 读取 JSON 文件
+            Gson gson = new Gson();
+            FileReader reader = new FileReader("src/main/java/top/playereg/ApiKeys.json");
+            JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+            String apiKey = jsonObject.get("sendApiKey").getAsString();
+
+            Resend resend = new Resend(apiKey);
+
+            CreateEmailOptions params = CreateEmailOptions.builder()
+                    .from(sendName + " <" + from + ">")
+                    .to(to)
+                    .subject(title)
+                    .html(text)
+                    .build();
+
+            CreateEmailResponse data = resend.emails().send(params);
+            System.out.println("邮件ID：" + data.getId());
+            return true;
+        } catch (ResendException e) {
+            e.printStackTrace();
+            return false;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
