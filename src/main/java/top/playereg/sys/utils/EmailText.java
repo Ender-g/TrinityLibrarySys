ca4d7dbfb5c38e1370cd935f0243894973fbe8a9
PlayerEG <enderg@qq.com>
2025-05-22 18:04:06 +0800

    @PlayerEG
    style(sys): 优化邮箱验证码 HTML 样式
    
    - 调整标题和验证码的样式，使其更加美观
    - 修改验证码字体大小为 50px，提高可读性
    - 调整各部分的布局，增加视觉上的层次感

--- src/main/java/top/playereg/sys/utils/EmailText.java
+++ src/main/java/top/playereg/sys/utils/EmailText.java
@@ -10,8 +10,9 @@ package top.playereg.sys.utils;
 
 public class EmailText {
     public static String code = (int) ((Math.random() * 9 + 1) * 100000) + ""; // 生成随机验证码
-    public static String text1 = "<h1 style=\"font-size: 18px\">Ciallo～(∠・ω< )⌒☆ 主人您的验证码是：</h1>" +
-            "<div style=\"font-size: 100px;text-align: center;margin-top: 100px;\">" + code + "</div>" +
+    public static String text1 = "<h1 style=\"font-size: 18px\">Ciallo～(∠・ω< )⌒☆</h1>" +
+            "<h1 style=\"font-size: 18px\">主人您的验证码是：</h1>" +
+            "<div style=\"font-size: 50px;text-align: center;margin-top: 70px;\">" + code + "</div>" +
             "<div style=\"font-size: 13px;text-align: center;margin-top: 100px;\">" +
             "请勿回复此邮件，此邮件为系统自动发送，请勿回复。</div>" +
             "<div style=\"font-size: 13px;text-align: center;margin-top: 20px;\">" +
