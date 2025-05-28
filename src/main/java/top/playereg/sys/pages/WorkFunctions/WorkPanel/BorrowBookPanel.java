735818cb88b3f78e945e33cf3816ad991d36f0e5
PlayerEG <enderg@qq.com>
2025-05-28 08:08:33 +0800

    @PlayerEG

--- src/main/java/top/playereg/sys/pages/WorkFunctions/WorkPanel/BorrowBookPanel.java
+++ src/main/java/top/playereg/sys/pages/WorkFunctions/WorkPanel/BorrowBookPanel.java
@@ -4,11 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class BorrowBookPanel extends JPanel {
-
         public BorrowBookPanel() {
             setLayout(null); // 保持与主框架一致的布局方式
             setBackground(Color.BLUE);
-            JLabel label = new JLabel("借阅图书界面");
+            JLabel label = new JLabel();
             label.setBounds(0, 0 ,1000, 500);
             add(label);
         }
