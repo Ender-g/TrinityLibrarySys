07458e29a798b1f0920934a0c8361804f648061f
PlayerEG <enderg@qq.com>
2025-06-18 20:33:18 +0800

    @PlayerEG

---
+++ src/main/java/top/playereg/sys/utils/MoreColors.java
@@ -0,0 +1,35 @@
+/*
+ *
+ * @author: playereg
+ * @description: 自定义颜色
+ * @version: 1.0
+ *
+ * */
+
+package top.playereg.sys.utils;
+
+import java.awt.*;
+
+public class MoreColors {
+    public static Color transparent = new Color(0x000000, true); // 透明
+    public static Color skyblue = new Color(0x87CEEB); // 天蓝色
+    public static Color blue = new Color(0x0000FF);
+    public static Color lightblue = new Color(0x00BFFF); // 亮蓝色
+    public static Color darkgreen = new Color(0x006400); // 深绿
+    public static Color lightgreen = new Color(0x7CFC00);// 亮绿色
+    public static Color darkred = new Color(0x8B0000);
+    public static Color lightred = new Color(0xFF0000);
+    public static Color darkyellow = new Color(0xFF8C00);
+    public static Color lightyellow = new Color(0xFFFF00);
+    public static Color darkgray = new Color(0xA9A9A9);
+    public static Color lightgray = new Color(0xD3D3D3);
+    public static Color darkorange = new Color(0xFF8C00);
+    public static Color lightorange = new Color(0xFFA500);
+    public static Color darkpurple = new Color(0x800080);
+    public static Color lightpurple = new Color(0xFF00FF);
+    public static Color darkblue = new Color(0x00008B);
+    public static Color darkpink = new Color(0xFF1493);
+    public static Color lightpink = new Color(0xFF69B4);
+    public static Color darkbrown = new Color(0xA52A2A);
+    public static Color lightbrown = new Color(0xDEB887);
+}
